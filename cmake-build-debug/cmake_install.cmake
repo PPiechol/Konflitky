e29ef7109dedaa6a6e404f84b00f3ef24dbde695
PPiechol <ul0266106@edu.uni.lodz.pl>
2023-03-02 21:26:30 +0100

    Dodanie funkcji for

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Users/Piotr Piechol/CLionProjects/Konflikt
+# Install script for directory: E:/Users/Piotr Piechol/CLionProjects/Nowy folder
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Users/Piotr Piechol/CLionProjects/Konflikt/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/Users/Piotr Piechol/CLionProjects/Nowy folder/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
